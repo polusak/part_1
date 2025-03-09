1d0fbeb8505073611333c5c0024f5290eb56ed45
Aku <ap@Aku-MacBook-Air.local>
2025-03-09 18:09:13 +0200

    1.5

--- kurssitiedot/src/App.jsx
+++ kurssitiedot/src/App.jsx
@@ -1,7 +1,7 @@
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
@@ -17,9 +17,9 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-  const part1 = props.parts[0]
-  const part2 = props.parts[1]
-  const part3 = props.parts[2]
+  const part1 = props.course.parts[0]
+  const part2 = props.course.parts[1]
+  const part3 = props.course.parts[2]
   return (
     <div>
       <Part part={part1.name} ex={part1.exercises}/>
@@ -30,9 +30,9 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  const ex1 = props.parts[0].exercises
-  const ex2 = props.parts[1].exercises
-  const ex3 = props.parts[2].exercises
+  const ex1 = props.course.parts[0].exercises
+  const ex2 = props.course.parts[1].exercises
+  const ex3 = props.course.parts[2].exercises
   const total = ex1 + ex2 + ex3
   return (
     <div>
@@ -42,27 +42,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
