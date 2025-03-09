36da31d82212df45368c46b0503a95c44427cef1
Aku <ap@Aku-MacBook-Air.local>
2025-03-09 17:47:07 +0200

    1.2

--- kurssitiedot/src/App.jsx
+++ kurssitiedot/src/App.jsx
@@ -6,20 +6,24 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.ex}
+      </p>
+    </div>
+  )
+}
+
 const Content = (props) => {
   const parts = props.parts
   const exercises = props.exercises
   return (
     <div>
-      <p>
-        {parts[0]} {exercises[0]}
-      </p>
-      <p>
-        {parts[1]} {exercises[1]}
-      </p>
-      <p>
-        {parts[2]} {exercises[2]}
-      </p>
+      <Part part={parts[0]} ex={exercises[0]}/>
+      <Part part={parts[1]} ex={exercises[1]}/>
+      <Part part={parts[2]} ex={exercises[2]}/>
     </div>
   )
 }
